13e8f1b92bad444c7505fccdab21da813d657b45
KMac <kimberly.mcdonald@edf-re.com>
2015-08-31 14:17:30 -0700

    Batch file created to launch Sublime, Sourcetree and live-server; CSS and JS taken out of HTML

---
+++ M2D2_Config.bat
@@ -0,0 +1,6 @@
+C:
+START "C:\Program Files (x86)\Atlassian\SourceTree\" "C:\Program Files (x86)\Atlassian\SourceTree\SourceTree.exe"
+START "C:\Program Files\Sublime Text 3" "C:/Program Files/Sublime Text 3/sublime_text.exe"
+G:
+cd G:/General/SP/R_and_D/Database/Z_GitHubKMac/m2d2-cleaningconfig
+live-server
---
+++ js/m2d2config.js
@@ -0,0 +1,19 @@
+/*!
+ * Bootstrap v3.3.5 (http://getbootstrap.com)
+ * Copyright 2011-2015 Twitter, Inc.
+ * Licensed under the MIT license
+ */
+
+$(function () {
+    $('#datetimepicker6').datetimepicker();
+    $('#datetimepicker7').datetimepicker({
+        useCurrent: false //Important! See issue #1075
+    });
+    $("#datetimepicker6").on("dp.change", function (e) {
+        $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
+    });
+    $("#datetimepicker7").on("dp.change", function (e) {
+        $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
+    });
+});
+
